4a1672fc4b03e0aaad547b2554c61e08378abdb7
quickly3 <quickly3@sohu.com>
2017-06-05 23:08:06 +0800

    no message

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { 
+	OnInit,
+	AfterViewChecked,
+	Component,
+	AfterViewInit 
+} from '@angular/core';
+
+
+import * as $ from 'jquery';
 import { AppMenu } from './layouts/app-menu.component';
 
 
@@ -8,6 +16,27 @@ import { AppMenu } from './layouts/app-menu.component';
 	styleUrls:['./app.component.scss']
 })
 
-export class App{
+export class App implements OnInit, AfterViewChecked, AfterViewInit{
+
+	ngOnInit() {
 	
+	}
+	
+	ngAfterViewInit() {
+		this.buildRightSide();
+		let foo = this.buildRightSide;
+		$(window).resize(foo);
+	}
+
+	ngAfterViewChecked(){
+		
+	}
+
+	buildRightSide(){
+		let _window_width = $("html").width();
+		let _menu_width = $("header").width();
+		let _right_side_width = _window_width - _menu_width - 5;
+		$(".rightSide").css({"width":_right_side_width+"px"});
+	}
+
 }
